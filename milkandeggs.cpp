851109a0b1d42cdc47924e951df5c42734109a40
nauski <nauski@nauski.com>
2016-06-01 16:50:09 +0300

    Added instructions and some basic functionality.

---
+++ milkandeggs.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstdlib>
+#include<string>
+
+using namespace std;
+int listanKoko = 50;
+int count = 0;
+
+void luoLista(string arr[], int size){
+
+  while (count < size) {
+      cout << "\nSyötä ostos: \n";
+      std::string temp;
+      cin >> temp;
+      arr[count] = temp;
+      count++;
+
+  //   tulostaLista(arr, count);
+      }
+
+}
+
+void tulostaLista(string arr[], int size){
+  std::cout << "\n" << std::endl;
+  std::cout << "Listalla: " << std::endl;
+
+  for(int i = 0; i < size; i++){
+    std::cout << arr[i]  << std::endl;
+  }
+}
+
+int main(void)
+{
+
+ std::cout << "=== Solukämppäsovellus ===\n" << std::endl;
+ std::cout << " - Ohjelmaan syötetään kolmen henkilön ostoslistat.\n";
+ std::cout << " - Syötä ostoksia listaan yksi kerrallaan.\n";
+ std::cout << " - Ostoksia voi olla maksimissaan 50 per henkilö.\n";
+ std::cout << " - Päätä kyseisen henkilön lista tyhjällä rivillä. \n";
+ std::cout << " - Kun viimeisen henkilön lista on kirjattu, \n";
+ std::cout << "   poistu ohjelmasta tyhjällä rivillä. \n" << std::endl;
+
+
+ string* hlo1 = new string [listanKoko];
+ string* hlo2 = new string [listanKoko];
+ string* hlo3 = new string [listanKoko];
+
+luoLista(hlo1, listanKoko);
+ //while (count < stringSize) {
+//     cout << "\nSyötä ostos: \n";
+//     std::string temp;
+//     cin >> temp;
+//     hlo1[count] = temp;
+//     count++;
+//
+//    tulostaLista(hlo1, count);
+//     }
+  //   delete[] s;
+}
